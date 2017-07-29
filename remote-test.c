ca39c65a0890c7ea8a6473d9dd9a9df1b6cd3fa6
KURURU <i@okako.com>
2017-07-30 08:06:09 +0900

    １

--- remote-test.c
+++ remote-test.c
@@ -1,4 +1,5 @@
 // cc -o remote remote.c -lm
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -6,11 +7,9 @@
 #include <sys/ioctl.h>
 #include <math.h>
 
-
 void main() {
 	
-   int xx,yy,ph,x,y,z,p,c1,c2,c3,c4,v1,v2,ww;
-
+   int v1,v2,ww,c1,c2,c3,c4;
 
     v1 = 0;    // 縦軸入力
     v2 = 0;    // 横軸入力
@@ -21,10 +20,11 @@ void main() {
     c3 = ( 8 * v1 + -8 * v2 +  6 * ww ) / 10;   // 左後
     c4 = ( 8 * v1 +  8 * v2 + -6 * ww ) / 10;   // 右後
 
-    printf(" c1=%4d ",c1);
-    printf(" c2=%4d ",c2);
-    printf(" c3=%4d ",c3);
-    printf(" c4=%4d ",c4);
+    printf(" 左前=%4d ",c1);
+    printf(" 右前=%4d ",c2);
+    printf("\n");
+    printf(" 左後=%4d ",c3);
+    printf(" 右後=%4d ",c4);
     printf("\n");
 
     
